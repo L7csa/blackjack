0b0174c0c29af0af0b48c4de8b84d6f0be158c6b
Scott Armstrong <collinsscottarmstrong@gmail.com>
2013-09-29 20:05:13 -0700

    starting code

--- blackjack.rb
+++ blackjack.rb
@@ -1,37 +1,109 @@
 # basic methods and defs
-  # say
-  # bust
-  # basic deck
+def say(s)
+  puts "==>   " + s.to_s
+end
 
-# get name
+# # bust value
+# bust = 21
 
-# init deck (with numbers)
+# basic deck
+deck52 = [ 2, 3, 4, 5, 6, 7, 8 ,9, 10, 10, 10, 11 ]
+
+# # get name
+# say "Welcome to TeaLeaf Blackjack. My name is Scott, and I will be your "
+# say "dealer today. What is your name?"
+# puts
+# name = gets.chomp
+# puts
+# say "Welcome, #{name}. Let's play some cards."
 
 # shuffle
 
+deck52.shuffle!
+p_hand = []
+d_hand = []
+p_val = 0
+d_val = 0
+
 # deal
 
-# display player value
+2.times { p_hand << deck52.pop }
+say p_hand
+2.times { d_hand << deck52.pop }
+say d_hand
 
-# ask for player action
-  # hit
-  # bust
-  # stand
+# # init deck (with numbers)
 
-# dealer action
+# def hand()
+#   deck52 = [ 2, 3, 4, 5, 6, 7, 8 ,9, 10, 10, 10, 11 ]
+# # shuffle
+#   deck52.shuffle!
+#   d_hand = []
+#   p_hand = []
+#   p_val = 0
+#   d_val = 0
 
-# Play again
+# # deal
+#   def deal()
+#     d_hand = deck52.pop(2)
+#     p_hand = deck52.pop(2)
+#   end
 
-def replay()
-  puts "Would you like to play again? Enter 'y' or 'n'."
-  replay = gets.chomp
-  if replay.to_s == 'y'
-  	exec("ruby blackjack.rb")
-  elsif replay.to_s == 'n'
-    puts "Thank you for playing!"
-  else
-    replay()
-  end
-end
+# # display player value
+#   def disp_p_hand()
+#     p_hand.each do |x|
+#     p_val += x.to_i
+#     say "You have #{p_hand.each { |y| puts y }}. Your total is #{p_val}."
+#     end
+#   end
+ 
+#   disp_p_hand()
 
-replay
+# # ask for player action
+#   bust = false
+#   def action()
+#     while bust == false
+#     say "What would you like to do -- hit (h) or stand (s)?"
+#     hs = gets.chomp
+
+#       if hs == "s"
+#         dealer()
+#       elsif hs == "h"
+#         p_hand += deck52.pop
+#         disp_p_hand
+#       else
+#         say "That is not a valid action."
+#         action()
+#       end
+#     end
+#   end
+
+#   action()
+
+  
+
+
+#   # hit
+#   # stand
+
+# # dealer action
+
+# end
+
+# hand()
+
+# # Play again
+
+# def replay()
+#   puts "Would you like to play again? Enter 'y' or 'n'."
+#   replay = gets.chomp
+#   if replay.to_s == 'y'
+#   	hand()
+#   elsif replay.to_s == 'n'
+#     puts "Thank you for playing!"
+#   else
+#     replay()
+#   end
+# end
+
+# replay
